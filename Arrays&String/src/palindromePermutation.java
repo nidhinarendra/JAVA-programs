13c7446c1db63135186b5e7cfa4b4c13a473c85b
Nidhi Narendra <nidhi.narendra1@gmail.com>
2017-01-28 21:12:59 -0800

    One edit away program fixed and working without bugs

---
+++ Arrays&String/src/palindromePermutation.java
@@ -0,0 +1,25 @@
+/*
+ * Palindrome Permutation: Given a string, write a function to check 
+ * if it is a permutation of a palindrome. 
+ * A palindrome is a word or phrase that is the same forwards and backwards. 
+ * A permutation is a rearrangement of letters. 
+ * The palindrome does not need to be limited to just dictionary words.
+ */
+public class palindromePermutation {
+
+	public static int getChar
+	
+	public static boolean isPP(String str){
+	int[] array = new int[26]; //Assuming the string has only alphabet and they are case insensitive (A=a)
+	for(char c : str.toCharArray()){
+		int x = getCharNum(c)
+	}
+	}
+	public static void main(String[] args) {
+		String str = "abcd";
+		boolean result = isPP(str);
+		System.out.println(result);
+	
+	}
+
+}
